34cc652e5f6688d19dfe3f7fe50c21c757d819f0
soareslil <lilianectd@gmail.com>
2023-03-27 15:50:09 -0300

    definindo metodos

--- main.js
+++ main.js
@@ -1,13 +1,43 @@
-let listaDeCompras = ['Arroz', 'Feijao', 'Suco']; 
+let listaDeItens = [];
 
-const listaDeItems = {
-    item1: 'Suco',
-    item2: 'Arroz',
-    quantidade1: 2,
-    quantidade2: 3,
-    mostrarItens : function(){
-        alert('comprei ' + listaDeItems.quantidade1 + ' pacotes de  ' + listaDeItems.item1);
-    }
+const form = document.getElementById("form-itens");
+const itensInput = document.getElementById("receber-item");
+const ulItens = document.getElementById("lista-de-itens");
+
+form.addEventListener("submit", function (evento) {
+  evento.preventDefault();
+  salvarItem();
+  mostrarItens();
+});
+
+function salvarItem() {
+  const comprasItem = itensInput.value;
+  const checarDuplicado = listaDeItens.some(
+    (elemento) => elemento.valor.toUpperCase() === comprasItem.toUpperCase()
+  );
+  if (checarDuplicado) {
+    alert("item ja existe");
+  } else {
+    listaDeItens.push({
+      valor: comprasItem,
+    });
+  }
+  console.log(listaDeItens);
 }
 
-listaDeItems.mostrarItens()
+function mostrarItens() {
+    ulItens.innerHTML = '';
+  listaDeItens.forEach((elemento, index) => {
+    ulItens.innerHTML += `
+        <li class="item-compra is-flex is-justify-content-space-between" data-value="${index}">
+        <div>
+            <input type="checkbox" class="is-clickable" />
+            <input type="text" class="is-size-5" value="${elemento.valor}"></input>
+        </div>
+        <div>
+            <i class="fa-solid fa-trash is-clickable deletar"></i>
+        </div>
+    </li>
+        `;
+  });
+}
